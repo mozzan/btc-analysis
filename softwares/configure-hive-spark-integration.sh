a2a61bc5cdaecb45c07c4ccdbbe560fade3c896e
chiahung.lin <chiahung.lin@rakuten.com>
2018-11-11 14:56:00 +0800

    remove unused files

--- softwares/configure-hive-spark-integration.sh
+++
@@ -1,5 +0,0 @@
-cp /usr/lib/hive/conf/hive-site.xml /usr/lib/spark/conf
-bdconfig set_property \
-  --configuration_file "/usr/lib/spark/conf/hive-site.xml" \
-  --name 'hive.execution.engine' --value 'mr' \
-  --clobber
