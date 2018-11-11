9cd37d071e01147123ad3aec82339bbc34c7af40
chiahung.lin <chiahung.lin@rakuten.com>
2018-11-11 14:50:45 +0800

    add jupyter, add connect, open command

--- softwares/manage-cluster.sh
+++ softwares/manage-cluster.sh
@@ -18,17 +18,7 @@ case $COMMAND in
     gcloud dataproc clusters create cluster-1 \
     --zone asia-northeast1-b \
     --initialization-actions=\
-gs://dataproc-initialization-actions/tez/tez.sh,\
-gs://btc-analysis-tw/softwares/configure-hive-spark-integration.sh,\
-gs://dataproc-initialization-actions/jupyter/jupyter.sh,\
-gs://btc-analysis-tw/softwares/create-database.sh \
-    --scopes sql,sql-admin \
-    --image-version 1.1 \
-    --metadata "hive-metastore-instance=btc-20181105:asia-east1:btc-analysis-tw" \
-    --properties=hive:hive.exec.dynamic.partition.mode=nonstrict,\
-hive:hive.exec.compress.output=true,\
-hive:mapred.output.compression.codec=com.hadoop.compression.lzo.LzoCodec,\
-hive:hive.metastore.warehouse.dir=gs://btc-analysis-tw/hive-warehouse \
+gs://dataproc-initialization-actions/jupyter/jupyter.sh \
     --initialization-action-timeout=30m \
     $WORKERS \
     --worker-machine-type=n1-standard-1 \
@@ -36,6 +26,12 @@ hive:hive.metastore.warehouse.dir=gs://btc-analysis-tw/hive-warehouse \
     ;;
   stop)
     yes | gcloud dataproc clusters delete cluster-1
+    ;;
+  connect)
+    gcloud compute ssh cluster-1-m --project=mozzan-world --zone=asia-northeast1-b -- -D 1080 -N
+    ;;
+  open)
+    "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"   --proxy-server="socks5://localhost:1080"   --user-data-dir="/tmp/cluster-1-m" http://cluster-1-m:8123
     ;;
   check_and_terminate)
     gcloud compute scp ~/git/btc-analysis/softwares/should_terminate_cluster.python cluster-1-m:
